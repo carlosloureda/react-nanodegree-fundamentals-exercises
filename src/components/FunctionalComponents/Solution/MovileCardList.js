6345b191ece4fbefa04d25c547785b7ca4d056e5
Carlos Loureda <balanzeneto@gmail.com>
2019-11-13 11:51:53 +0100

    Solution for Functional Components Exercise

--- src/components/FunctionalComponents/Solution/MovileCardList.js
+++ src/components/FunctionalComponents/Solution/MovileCardList.js
@@ -1,41 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import MovieCard from "./MovileCard";
 
-class MovieCardsList extends Component {
-  render() {
-    /*
-  Destructuring via ES6. We're getting the profiles, users, and movies properties
-  off of the pros passed into this presentational component. If you need a refresher on this syntax, check
-  out this course: https://www.udacity.com/course/es6-javascript-improved--ud356
-  */
+const MovieCardsList = ({ profiles, users, movies }) => {
+  const usersByMovie = {};
 
-    const { profiles, users, movies } = this.props;
-    const usersByMovie = {};
+  profiles.forEach(profile => {
+    const movieID = profile.favoriteMovieID;
 
-    profiles.forEach(profile => {
-      const movieID = profile.favoriteMovieID;
+    if (usersByMovie[movieID]) {
+      usersByMovie[movieID].push(profile.userID);
+    } else {
+      usersByMovie[movieID] = [profile.userID];
+    }
+  });
 
-      if (usersByMovie[movieID]) {
-        usersByMovie[movieID].push(profile.userID);
-      } else {
-        usersByMovie[movieID] = [profile.userID];
-      }
-    });
-
-    const movieCards = Object.keys(movies).map(id => (
-      <MovieCard
-        key={id}
-        users={users}
-        usersWhoLikedMovie={usersByMovie[id]}
-        movieInfo={movies[id]}
-      />
-    ));
-
-    /*
-    Return JSX
-    */
-    return <ul>{movieCards}</ul>;
-  }
-}
+  const movieCards = Object.keys(movies).map(id => (
+    <MovieCard
+      key={id}
+      users={users}
+      usersWhoLikedMovie={usersByMovie[id]}
+      movieInfo={movies[id]}
+    />
+  ));
+  return <ul>{movieCards}</ul>;
+};
 
 export default MovieCardsList;
