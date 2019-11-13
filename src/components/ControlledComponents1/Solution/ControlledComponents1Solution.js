e19c3a418029c262908f068a0ce4d61504350426
Carlos Loureda <balanzeneto@gmail.com>
2019-11-13 12:55:48 +0100

    Added Solution for Controlled Components Exercise 1

--- src/components/ControlledComponents1/Solution/ControlledComponents1Solution.js
+++ src/components/ControlledComponents1/Solution/ControlledComponents1Solution.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 
 class ControlledComponents1Solution extends Component {
+  state = {
+    value: ""
+  };
+
+  onHandleChange = e => {
+    this.setState({ value: e.target.value });
+  };
   render() {
     return (
       <div className="container">
-        <input type="text" placeholder="Say Something" />
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.onHandleChange}
+          placeholder="Say Something"
+        />
         <p className="echo">Echo:</p>
-        <p>This should mirror the text you typed into the input field.</p>
+        <p>{this.state.value}</p>
       </div>
     );
   }
